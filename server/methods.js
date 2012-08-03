536b4217619826612708fc94a1f5bf7d4125c9ea
Felix <felix@stkn.org>
2012-08-03 20:39:26 +0200

    rm unneeded var

--- server/methods.js
+++ server/methods.js
@@ -2,7 +2,7 @@ Meteor.methods({
 	comment: function(name, message, date) {
 		if(message) {
 			if(!name) {
-				var name = "Anonymous";
+				name = "Anonymous";
 			}
 			Posts.insert({name: name, message: message, date: date});
 			return true;
