f0fe08576847d17c7ce2c23dc4c5e31b91ce6304
Felix <felix@stkn.org>
2012-08-08 12:45:17 +0200

    tabs -> 2 spaces

--- client/metapp.js
+++ client/metapp.js
@@ -4,46 +4,45 @@ Meteor.subscribe('allposts');
 
 // Output posts
 Template.section.posts = function () {
-	return Posts.find();
+  return Posts.find();
 };
 
 // Events for Template Section
 Template.section.events = {
-	// Call Comment Submit
-	'click input.submit': function () {
-		Meteor.call('comment',
-			$('input#name').val(),
-			$('input#post').val(),
-			(new Date()).getTime(),
-			function (error, result) {
-				if (!result) {
-					$('input#post').attr('placeholder',
-					'please type a message...');
-				}
-				else if (!error) {
-					$('input#name').val('');
-					$('input#post').val('');
-				}
-			}
-		);
-	},
-	'click a.delete': function () {
-		Posts.remove(Session.get('selected_post'));
-	}
+  // Call Comment Submit
+  'click input.submit': function () {
+    Meteor.call('comment',
+      $('input#name').val(),
+      $('input#post').val(),
+      (new Date()).getTime(),
+      function (error, result) {
+        if (!result) {
+          $('input#post').attr('placeholder', 'please type a message...');
+        }
+        else if (!error) {
+          $('input#name').val('');
+          $('input#post').val('');
+        }
+      }
+    );
+  },
+  'click a.delete': function () {
+    Posts.remove(Session.get('selected_post'));
+  }
 };
 
 // Get Username for selected Post
 Template.section.selected_post = function () {
-	var post = Posts.findOne(Session.get('selected_post'));
-	return post && post.name;
+  var post = Posts.findOne(Session.get('selected_post'));
+  return post && post.name;
 };
 
 // Select a Post
 Template.post.selected = function () {
-	return Session.equals('selected_post', this._id) ? 'selected' : '';
+  return Session.equals('selected_post', this._id) ? 'selected' : '';
 };
 Template.post.events = {
-	'click': function () {
-		Session.set('selected_post', this._id);
-	}
+  'click': function () {
+    Session.set('selected_post', this._id);
+  }
 };
--- server/methods.js
+++ server/methods.js
@@ -1,12 +1,12 @@
 Meteor.methods({
-	comment: function (name, message, date) {
-		if (message) {
-			if (!name) {
-				name = 'Anonymous';
-			}
-			Posts.insert({name: name, message: message, date: date});
-			return true;
-		}
-		return false;
-	}
+  comment: function (name, message, date) {
+    if (message) {
+      if (!name) {
+        name = 'Anonymous';
+      }
+      Posts.insert({name: name, message: message, date: date});
+      return true;
+    }
+    return false;
+  }
 });
