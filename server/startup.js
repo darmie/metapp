583eb458105f7f2f95c27b2a0951b3a344116dda
Felix <felix@stkn.org>
2012-08-11 14:25:55 +0200

    fix startup

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,12 @@
 Meteor.startup(function () {
- 
+  if(News.find().count() === 0) {
+    News.insert({title: "Test News 1", article: "bla bla bla"});
+    News.insert({title: "Test News 2", article: "bla bla bla"});
+    News.insert({title: "Test News 3", article: "bla bla bla"});
+    News.insert({title: "Test News 4", article: "bla bla bla"});
+    News.insert({title: "Test News 5", article: "bla bla bla"});
+    News.insert({title: "Test News 6", article: "bla bla bla"});
+    News.insert({title: "Test News 7", article: "bla bla bla"});
+    News.insert({title: "Test News 8", article: "bla bla bla"});
+  }
 });
