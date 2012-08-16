74133ea4ffe01646c9824181aa83e45174e0cefb
Felix <felix@stkn.org>
2012-08-16 19:07:54 +0200

    fixed autosubscribe

--- client/metapp.js
+++ client/metapp.js
@@ -1,11 +1,9 @@
 //Suscribes
-Meteor.autosubscribe(function () {
-  Meteor.subscribe('news');
-});
+Meteor.subscribe('news');
 Meteor.autosubscribe(function () {
   var news_id = Session.get('news_id');
   if (news_id) {
-    Meteor.subscribe('comments', Session.get('news_id'));
+    Meteor.subscribe('comments', news_id);
   }
 });
 
--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,6 @@
 Meteor.publish('news', function () {
   return News.find();
 });
-
 Meteor.publish('comments', function (news_id) {
   return Comments.find({news_id: news_id});
 });
